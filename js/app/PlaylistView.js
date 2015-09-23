9875a7770ff3ddd502483cd13c60a2eb5377172d
JaZahn <jcleveng@fas.harvard.edu>
2015-09-23 17:51:59 -0400

    finishing week3 solution

--- js/app/PlaylistView.js
+++ js/app/PlaylistView.js
@@ -1,20 +1,28 @@
 define(['jquery', 'Playlist'], function($, Playlist){
 
-  var PlaylistView = function(){
+  var PlaylistView = function(config){
+    // since we're doing work with the DOM,
+    //   we want to abstract out those pieces to make it testable
+    // we can do this with a config
+    this.config = config || {};
+    this.$addSongForm = this.config.addSongForm || $('#addSongForm');
+    this.$song = this.config.song || $('#song');
+    this.$currentPlaylist = this.config.currentPlaylist || $('#currentPlaylist');
+
     // initialize
     this.playlist = new Playlist();
+    this.addSong = $.proxy(this.addSong, this);
     this.listenAddSong();
-
     this.updatePlaylist();
+
   };
   PlaylistView.prototype.listenAddSong = function(){
-    var that = this;
-    $('#addSongForm').on('submit', $.proxy(this.addSongListener, this));
+    this.$addSongForm.on('submit', this.addSong);
   };
-  PlaylistView.prototype.addSongListener = function(event){
-    this.playlist.addSong($('#song').val());
+  PlaylistView.prototype.addSong = function(event){
+    this.playlist.addSong(this.$song.val());
     this.updatePlaylist();
-    $('#song').val('');
+    this.$song.val('');
     return false;
   };
 
@@ -23,14 +31,13 @@ define(['jquery', 'Playlist'], function($, Playlist){
     this.updatePlaylistDom();
   };
   PlaylistView.prototype.updatePlaylistDom = function(){
-    var that = this;
     var playlistDom = this.playlist.playlist.map($.proxy(this.createPlaylistDomMap, this));
 
-    $('#currentPlaylist').html(playlistDom);
-
+    this.$currentPlaylist.html(playlistDom);
   };
 
   PlaylistView.prototype.createPlaylistDomMap = function(song, index){
+    var that = this;
     var removeButton = document.createElement("button");
     removeButton.appendChild(document.createTextNode("remove"));
     $(removeButton).click(function(){
